5d1a98158cd8d2874994de453a8d1a5d522a7189
alexzhong22c <535377683@qq.com>
2017-07-12 11:32:16 +0800

    post-janpan-style

---
+++ js/styleData.js
@@ -0,0 +1,39 @@
+var japanStyle = [
+  {
+    id: 'js00',
+    name: '白绿',
+    hex: '#A8D8B9'
+  }, {
+    id: 'js01',
+    name: '薄青',
+    hex: '#91B491'
+  }, {
+    id: 'js02',
+    name: '青瓷',
+    hex: '#69B0AC'
+  }, {
+    id: 'js03',
+    name: '苗',
+    hex: '#86C166'
+  }, {
+    id: 'js04',
+    name: '若竹',
+    hex: '#5DAC81'
+  }, {
+    id: 'js05',
+    name: '青绿',
+    hex: '#00AA90'
+  }, {
+    id: 'js06',
+    name: '柳染',
+    hex: '#91AD70'
+  }, {
+    id: 'js07',
+    name: '苔',
+    hex: '#838A2D'
+  }, {
+    id: 'js08',
+    name: '松叶',
+    hex: '#42602D'
+  }
+]
